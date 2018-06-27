d1e828de0a07f0a9dd34effbedca922af6ac2b34
Jane Costa <yberdysh@gmail.com>
2018-06-27 17:28:31 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,29 @@
 require 'pry'
 
 class String
+	# def initialize
+	# end
 
   def sentence?
+  	self[self.length - 1] == "."
     
   end
 
   def question?
-
+	self[self.length - 1] == "?"
   end
 
   def exclamation?
-
+  	self[self.length - 1] == "!"
   end
 
   def count_sentences
+	@counter = 0
+	array = self.split(" ")
 
+  	array.each do |i|
+  		(i.include?(".") || i.include?("?") || i.include?("!")) && @counter += 1 
+  	end 
+	  @counter
   end
 end
